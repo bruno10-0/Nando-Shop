06ef359553d5c94705c2bd9adcfafd38f14f063c
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2023-12-19 19:14:48 -0300

    style(navbar): :fire: Elimine espacio en blanco

--- src/app/layouts/Navbar/Navbar.tsx
+++ src/app/layouts/Navbar/Navbar.tsx
@@ -2,34 +2,28 @@
 import Dropdown from "@/components/dropdown/Dropdown";
 import { themes } from "./dataNavbar";
 import InputTheme from "./compsNavbar/InputTheme";
-import { useState } from "react";
 import SearchbarNavbar from "./compsNavbar/SearchbarNavbar";
 import style from "./Navbar.module.css"
 import useScroll from "@/utils/hooks/useScroll";
+
 const Navbar = () => {
-    // const [ scrollUp, setScrollUp ] = useState(true);
+    
     const { scrollUp } = useScroll();
 
     return (
         <div className="flex flex-column">
             <div className={`bg-base-100 z-[10] fixed transition-all duration-500 w-full justify-end flex ${scrollUp ? 'translate-y-0 opacity-100':'-translate-y-full opacity-0'}`} >
-
-                
-
                 <div className={ `${ style.NavbarOptions } flex-none` }>
 
-                    
                     <Dropdown title="Temas" listClassName="overflow-y-auto overflow-x-hidden max-h-[calc(100vh)] h-[28.6rem] inline-flex flex-col" containerClassName="p-0" btnClassName="min-h-min h-min py-2">
                         {themes.map((theme, index) => {
                             return <InputTheme key={index} ariaLabel={ theme } value={ theme }/>
                         })}
                     </Dropdown>
-                    
                 </div>
             </div>
             <div className="flex z-[0]">
                 <SearchbarNavbar scrollUp={scrollUp}/>
-
             </div>
         </div>
     )
