463d55b1d2fe2db57c3a86c0ed3e6b6de4618ffe
bruno10-0 <brunobrodon75@gmail.com>
2023-12-27 22:56:33 -0300

    feat: :lipstick: cree un nuevo tema

--- src/app/layouts/Navbar/dataNavbar.ts
+++ src/app/layouts/Navbar/dataNavbar.ts
@@ -31,5 +31,6 @@ export const themes = [
     "dim",
     "nord",
     "sunset",
-    "nando"
+    "nando",
+    "bruno"
   ]
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -84,6 +84,19 @@ const config: Config = {
           "warning": "#FAE122",
           "error": "#e11d48",
         },
+        "bruno": {
+          "primary": "#0B0E37",
+          "secondary": "#2654A1",
+          "accent": "#3B0078",
+          "neutral": "#09090C",
+          "base-100": "#1E1E1E",
+          "info": "#137D9E",
+          "success": "#00B33C",
+          "warning": "#CCAA00",
+          "error": "#B50032"
+        }
+        
+        
       }
     ],
   },
